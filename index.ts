8fffceb93412ca1abcdf58ad8cd04851ef3c9d9f
vd <viridian035@gmail.com>
2023-07-24 19:04:28 +0200

    add cancel button

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import {
 	Button,
 	ButtonStyles,
 	HStack,
-	Input, State, TextInputCfg,
+	Input, Spacer, State, TextInputCfg,
 	VStack
 } from '@frugal-ui/base';
 import FilePicker, {Types} from '@frugal-ui/fsexpress-filepicker';
@@ -13,18 +13,29 @@ export default function SaveDialog(
 	rootName: string,
 	initialDirectory: string,
 	save: SaveFn,
+	cancel: () => void,
 ) {
 	const fileName = new State('');
 	const openDirectory = new State(initialDirectory);
 
 	return VStack(
 		HStack(
+			Spacer(),
+
 			Input(new TextInputCfg(fileName, 'Untitled')),
 			Button({
 				style: ButtonStyles.Primary,
 				accessibilityLabel: 'save here',
 				text: 'Save',
 				action: () => save(`${openDirectory.value}/${fileName.value}`),
+			}),
+
+			Spacer(),
+
+			Button({
+				accessibilityLabel: 'cancel',
+				iconName: 'close',
+				action: cancel,
 			}),
 		)
 			.useDefaultPadding()
